86f05e920425e86d454d0784feca8d6034f36286
amirvalhalla <amirvalhalla@proton.me>
2024-07-16 22:57:57 +0330

    feat: initialize project

---
+++ README.md
@@ -0,0 +1,21 @@
+# ProxyMiner
+
+ProxyMiner is a powerful tool designed to scrape and collect proxies from various websites. It automates the process of gathering fresh proxy lists and saves them to a file for easy access and use.
+
+## Features
+
+- **Automated Scraping**: Continuously scrape multiple websites for the latest proxies.
+- **Customizable Sources**: Easily add or remove proxy sources to tailor the scraping process to your needs.
+- **Proxy Validation**: Ensure collected proxies are functional and reliable.
+- **Output File**: Store the gathered proxies in a file, formatted for easy integration with other tools and applications.
+- **Scheduling**: Schedule scraping tasks to run at specific intervals, ensuring your proxy list is always up-to-date.
+
+## Installation
+
+Clone the repository and install the required dependencies:
+
+```bash
+git clone https://github.com/yourusername/ProxyMiner.git
+cd ProxyMiner
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,39 @@
+"""Python setup.py for ProxyMiner package"""
+import io
+import os
+from setuptools import find_packages, setup
+
+
+def read(*paths, **kwargs):
+    """Read the contents of a text file safely."""
+    content = ""
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *paths),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+
+
+def read_requirements(path):
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(('"', "#", "-", "git+"))
+    ]
+
+
+setup(
+    name="ProxyMiner",
+    version="0.0.1",
+    description="ProxyMiner is a powerful tool designed to scrape and collect proxies from various websites. It automates the process of gathering fresh proxy lists and saves them to a file for easy access and use.",
+    url="https://github.com/amirvalhalla/ProxyMiner",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="amirvalhalla",
+    packages=find_packages(exclude=["tests", ".github"]),
+    install_requires=read_requirements("requirements.txt"),
+    entry_points={
+        "console_scripts": ["ProxyMiner = ProxyMiner.__main__:main"]
+    },
+)
